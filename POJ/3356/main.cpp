deaa626f648dd69eb4dda1aab8fd907e4ffe3fba
Dalong Cheng <yuguess@gmail.com>
2014-04-07 22:08:00 -0500

    AC 3356, premature optmization is dangerous

--- POJ/3356/main.cpp
+++ POJ/3356/main.cpp
@@ -10,10 +10,28 @@ using namespace std;
 char A[MAX];
 char B[MAX];
 
+int a[MAX][MAX];
+
 int main() {
   int la = 0, lb = 0;
-  scanf("%d %s", &la, A);
-  scanf("%d %s", &lb, B);
 
+  while (cin >> la >> A + 1 >> lb >> B + 1) {
+    int minD = 0;
 
+    for (int i = 0; i < MAX; i++) {
+      a[0][i] = i;
+      a[i][0] = i;
+    }
+
+    for (int i = 1; i <= la; i++)
+      for (int j = 1; j <= lb; j++) {
+        if (A[i] == B[j])
+          a[i][j] = min(a[i - 1][j - 1], min(a[i][j - 1] + 1, a[i - 1][j] + 1));
+        else
+          a[i][j] = min(a[i - 1][j], min(a[i][j - 1], a[i - 1][j - 1])) + 1;
+      }
+
+    printf("%d\n", a[la][lb]);
+    memset(a, 0, sizeof(int) * MAX * MAX);
+  }
 }
