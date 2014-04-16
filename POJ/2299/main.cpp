11f13a4ffb91d074886236cc2d33bb62185cc484
Dalong Cheng <yuguess@gmail.com>
2014-04-16 17:31:58 -0500

    ready to solve a binary index tree problem

---
+++ POJ/2299/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main() {
+  int n = 0;
+
+  while (scanf("%d", &n) && n) {
+
+  }
+}
+
