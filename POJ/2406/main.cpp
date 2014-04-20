a228bbf0cd084e03e8b1229762cd53efbffb4859
Dalong Cheng <yuguess@gmail.com>
2014-04-20 18:19:50 -0500

    WA in 2406, test the data

---
+++ POJ/2406/main.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <map>
+using namespace std;
+
+#define MAX 1000010
+char s[MAX];
+char F[MAX];
+int l = 0;
+
+void buildFailure() {
+  int j = 0;
+  F[0] = F[1] = 0;
+
+  for (int i = 2; i <= l; i++) {
+    j = F[i - 1]; 
+
+    while (1) {
+      if (s[j] == s[i - 1]) {
+        F[i] = j + 1;
+        break;
+      }
+
+      if (j == 0) {
+        F[j] = 0; 
+        break;
+      }
+
+      j = F[j];
+    }
+  }
+}
+
+int main() {
+  
+  while (scanf("%s", s) && strcmp(s, ".")) {
+    l = strlen(s);  
+    buildFailure();
+
+    if (l % (l - F[l]) == 0) {
+      printf("%d\n", l / (l - F[l]));
+    } else {
+      printf("1\n");
+    }
+  }
+}
