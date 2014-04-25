044a33ba7c2f1561c8a73587a0fed3a17f7df273
Dalong Cheng <yuguess@gmail.com>
2014-04-25 16:33:20 -0500

    ready to AC 1386

---
+++ POJ/1386/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <map>
+using namespace std;
+
+int main() {
+  int nc = 0;
+
+  scanf("%d", &nc);
+
+  while (nc--) {
+
+  }
+}
