4f66a125b6bb9531518de916db1cd05f9591a7c3
Dalong Cheng <yuguess@gmail.com>
2014-05-04 17:03:44 -0500

    a trivial update

--- POJ/2985/main.cpp
+++ POJ/2985/main.cpp
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main() {
+  int M = 0, N = 0, op = 0;
+  scanf("%d %d", &N, &M);
+  for (int i = 0; i < M; i++) {
+    scanf("%d", &op);
 
+    if (op == 0) {
+
+    } else {
+
+    }
+  }
 }
